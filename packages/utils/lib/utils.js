114093bce35ca1462cb4ce7602f2b9d62d51ee17
石头 <27911624+zl19921218@users.noreply.github.com>
2022-08-31 15:50:21 +0800

    publish

--- packages/utils/lib/utils.js
+++ packages/utils/lib/utils.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: 小石头
+ * @Date: 2022-08-31 14:57:15
+ * @LastEditors: 小石头
+ * @LastEditTime: 2022-08-31 15:49:29
+ * @Description: 
+ */
 'use strict';
 
 module.exports = utils;
@@ -5,3 +12,5 @@ module.exports = utils;
 function utils() {
     return "Hello from utils";
 }
+
+
