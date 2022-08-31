b5f797da46d62af1a54106cfafb41c0d6b2caa97
石头 <27911624+zl19921218@users.noreply.github.com>
2022-08-31 15:55:35 +0800

    提交代码

--- packages/utils/lib/utils.js
+++ packages/utils/lib/utils.js
@@ -2,7 +2,7 @@
  * @Author: 小石头
  * @Date: 2022-08-31 14:57:15
  * @LastEditors: 小石头
- * @LastEditTime: 2022-08-31 15:49:29
+ * @LastEditTime: 2022-08-31 15:55:09
  * @Description: 
  */
 'use strict';
@@ -12,5 +12,4 @@ module.exports = utils;
 function utils() {
     return "Hello from utils";
 }
-
 
